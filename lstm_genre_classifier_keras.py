d47ce5cf3eb03587f686d633f553a4477c0801d3
Iroro Orife <iorife@netflix.com>
2019-05-09 23:08:39 -0700

    [ADD] blurb to really explain how things are setup and laid out, fix nonesense black-delinting, use num_epochs cos its easier to understand

--- lstm_genre_classifier_keras.py
+++ lstm_genre_classifier_keras.py
@@ -1,3 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    Keras implementation of a simple 2-layer-deep LSTM for genre classification of musical audio.
+    Feeding the LSTM stack are spectral {centroid, contrast}, chromagram and MFCC features
+
+    Model Summary:
+    _________________________________________________________________
+    Layer (type)                 Output Shape              Param #
+    =================================================================
+    lstm_1 (LSTM)                (None, 128, 128)          82944
+    _________________________________________________________________
+    lstm_2 (LSTM)                (None, 32)                20608
+    _________________________________________________________________
+    dense_1 (Dense)              (None, 8)                 264
+    =================================================================
+    Total params: 103,816
+    Trainable params: 103,816
+    Non-trainable params: 0
+
+    X shape (total # of training examples, sequence_length, input_dim)
+    Y shape (total # of training examples, # output classes)
+    ________________________________
+    Training X shape: (420, 128, 33)
+    Training Y shape: (420, 8)
+    ________________________________
+    Dev X shape: (120, 128, 33)
+    Dev Y shape: (120, 8)
+    ________________________________
+    Test X shape: (60, 128, 33)
+    Test Y shape: (60, 128, 33)
+
+    420 is the total number of 30 second training files, across all genres. From each file, we
+    extract a 128-length sequence of inputs (it could be longer). Each input is 33 element column
+    vector comprising mfcc, centroid, contrast & chroma features.
+
+    An epoch, containing all training data (420 sequences), is divided into 12 mini-batches of
+    length 35. Each mini-batch has 35 sequences. An LSTM RNN (stack) loops over each sequence
+    for sequence_length (128) steps computing an output value (of dimension 32) which is
+    transformed (via Dense layer) into 8 output classes
+"""
+
 import logging
 import os
 from keras.models import Sequential
@@ -34,8 +77,8 @@ else:
 # SGD    : lr=0.01, momentum=0., decay=0.
 opt = Adam()
 
-batch_size = 35
-nb_epochs = 400
+batch_size = 35  # num of training examples per minibatch
+num_epochs = 400
 
 print("Training X shape: " + str(genre_features.train_X.shape))
 print("Training Y shape: " + str(genre_features.train_Y.shape))
@@ -47,16 +90,9 @@ print("Test Y shape: " + str(genre_features.test_X.shape))
 input_shape = (genre_features.train_X.shape[1], genre_features.train_X.shape[2])
 print("Build LSTM RNN model ...")
 model = Sequential()
-model.add(
-    LSTM(
-        units=128,
-        dropout=0.05,
-        recurrent_dropout=0.35,
-        return_sequences=True,
-        input_shape=input_shape,
-    )
-)
-model.add(LSTM(units=32, dropout=0.05, recurrent_dropout=0.35, return_sequences=False))
+
+model.add(LSTM(units=128, dropout=0.05, recurrent_dropout=0.35, return_sequences=True, input_shape=input_shape))
+model.add(LSTM(units=32,  dropout=0.05, recurrent_dropout=0.35, return_sequences=False))
 model.add(Dense(units=genre_features.train_Y.shape[1], activation="softmax"))
 
 print("Compiling ...")
@@ -68,7 +104,7 @@ model.fit(
     genre_features.train_X,
     genre_features.train_Y,
     batch_size=batch_size,
-    epochs=nb_epochs,
+    epochs=num_epochs,
 )
 
 print("\nValidating ...")
