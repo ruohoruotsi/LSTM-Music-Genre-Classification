c0da3ebfee2cdafd9c1c1404d00955ed3bc6eda9
Iroro Orife <iorife@netflix.com>
2019-05-16 11:19:50 -0700

    [CLEANUP] add comments, remove debugging prints, clean up layout to better match the keras version of this code

--- lstm_genre_classifier_pytorch.py
+++ lstm_genre_classifier_pytorch.py
@@ -24,10 +24,6 @@ from GenreFeatureData import (
     GenreFeatureData,
 )  # local python class with Audio feature extraction (librosa)
 
-
-batch_size = 35  # num of training examples per minibatch
-num_epochs = 400
-
 genre_features = GenreFeatureData()
 
 # if all of the preprocessed files do not exist, regenerate them all for self-consistency
@@ -45,31 +41,21 @@ else:
     print("Preprocessing raw audio files")
     genre_features.load_preprocess_data()
 
+train_X = torch.from_numpy(genre_features.train_X).type(torch.Tensor)
+test_X = torch.from_numpy(genre_features.test_X).type(torch.Tensor)
+
+# Targets is a long tensor of size (N,) which tells the true class of the sample.
+train_Y = torch.from_numpy(genre_features.train_Y).type(torch.LongTensor)
+test_Y = torch.from_numpy(genre_features.test_Y).type(torch.LongTensor)
+
 # Convert {training, test} torch.Tensors
-print(genre_features.train_X.shape)
-print(genre_features.test_X.shape)
-print(genre_features.train_Y.shape)
-print(genre_features.test_Y.shape)
+print("Training X shape: " + str(genre_features.train_X.shape))
+print("Training Y shape: " + str(genre_features.train_Y.shape))
+print("Test X shape: " + str(genre_features.test_X.shape))
+print("Test Y shape: " + str(genre_features.test_Y.shape))
 
-X_train = torch.from_numpy(genre_features.train_X).type(torch.Tensor)
-X_test = torch.from_numpy(genre_features.test_X).type(torch.Tensor)
-y_train = torch.from_numpy(genre_features.train_Y).type(torch.LongTensor)   # Target is a long tensor of size (N,) which tells the true class of the sample.
-y_test = torch.from_numpy(genre_features.test_Y).type(torch.LongTensor)     # Target is a long tensor of size (N,) which tells the true class of the sample.
-
-print(X_train.shape)
-print(X_test.shape)
-print(y_train.shape)
-print(y_test.shape)
-
-def get_accuracy(logit, target, batch_size):
-    ''' Obtain accuracy for training round '''
-    corrects = (torch.max(logit, 1)[1].view(target.size()).data == target.data).sum()
-    accuracy = 100.0 * corrects/batch_size
-    return accuracy.item()
-
-# class definition here
+# class definition
 class LSTM(nn.Module):
-
     def __init__(self, input_dim, hidden_dim, batch_size, output_dim=8, num_layers=2):
         super(LSTM, self).__init__()
         self.input_dim = input_dim
@@ -84,43 +70,47 @@ class LSTM(nn.Module):
         self.linear = nn.Linear(self.hidden_dim, output_dim)
 
     def init_hidden(self):
-        return (torch.zeros(self.num_layers, self.batch_size, self.hidden_dim),
-                torch.zeros(self.num_layers, self.batch_size, self.hidden_dim))
+        return (
+            torch.zeros(self.num_layers, self.batch_size, self.hidden_dim),
+            torch.zeros(self.num_layers, self.batch_size, self.hidden_dim),
+        )
 
     def forward(self, input):
+        # lstm step, only take output from the final sequence timetep to stuff into linear
         lstm_out, hidden = self.lstm(input)
+        input_to_linear = lstm_out[-1]
 
-        # Only take the output from the final timetep
-        # input_to_linear = lstm_out[-1].view(self.batch_size, -1)
-        input_to_linear = lstm_out[-1]  # grab last element of sequence to stuff into linear
-
-        # print(input_to_linear.shape)
         y_pred = self.linear(input_to_linear)
-        # print(y_pred.view(-1).shape)
-        # return y_pred.view(-1)
-
         genre_scores = F.log_softmax(y_pred, dim=1)
         return genre_scores
 
-lstm_input_size = 33
-hidden_layer_size = 128
-output_dimension = 8
-num_layers = 2
-learning_rate = 0.001
+    def get_accuracy(self, logits, target):
+        """ compute accuracy for training round """
+        corrects = (torch.max(logits, 1)[1].view(target.size()).data == target.data).sum()
+        accuracy = 100.0 * corrects / self.batch_size
+        return accuracy.item()
 
-num_batches = int(X_train.shape[0] / batch_size)    # all training data (epoch) / batch_size == num_batches (12)
+
+batch_size = 35  # num of training examples per minibatch
+num_epochs = 400
 
 # Define model
 print("Build LSTM RNN model ...")
-model = LSTM(lstm_input_size, hidden_layer_size, batch_size=batch_size,
-             output_dim=output_dimension, num_layers=num_layers)
+model = LSTM(
+    input_dim=33,
+    hidden_dim=128,
+    batch_size=batch_size,
+    output_dim=8,
+    num_layers=2,
+)
 loss_function = nn.NLLLoss()
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-# Train
+
 print("Training ...")
 
-hist = np.zeros(num_epochs)
+# all training data (epoch) / batch_size == num_batches (12)
+num_batches = int(train_X.shape[0] / batch_size)  
 
 for epoch in range(num_epochs):
     train_running_loss = 0.0
@@ -128,36 +118,37 @@ for epoch in range(num_epochs):
 
     # Init hidden state - if you don't want a stateful LSTM (between epochs)
     model.hidden = model.init_hidden()
-    for i in range(num_batches):  # for all 420 sequences
+    for i in range(num_batches):
 
         # zero out gradient, so they don't accumulate btw epochs
         model.zero_grad()
 
-        # Local batches and labels
-        X_local, y_local = X_train[i * batch_size:(i + 1) * batch_size, ], \
-                           y_train[i * batch_size:(i + 1) * batch_size, ]
+        # train_X shape(total # of training examples, sequence_length, input_dim)
+        # train_Y shape(total # of training examples, # output classes)
+        #
+        # Slice out local minibatches & labels => Note that we *permute* the local minibatch to
+        # match the PyTorch expected input tensor format of (sequence_length, batch size, input_dim)
+        X_local_minibatch, y_local_minibatch = (
+            train_X[i * batch_size: (i + 1) * batch_size,],
+            train_Y[i * batch_size: (i + 1) * batch_size,]
+        )
 
-        # massage input & targets to "match" what the loss_function wants
-        X_local = X_local.permute(1, 0, 2)
-        # print("y_local shape, before:")
-        # print(y_local.shape)
-        y_local = torch.max(y_local, 1)[1]  # NLLLoss does not expect a one-hot encoded vector as the target, but class indices
+        # Reshape input & targets to "match" what the loss_function wants
+        X_local_minibatch = X_local_minibatch.permute(1, 0, 2)
 
-        # print("y_local shape, after:")
-        # print(y_local.shape)
+        # NLLLoss does not expect a one-hot encoded vector as the target, but class indices
+        y_local_minibatch = torch.max(y_local_minibatch, 1)[1]
 
-        y_pred = model(X_local)                 # fwd the bass (forward pass)
-        loss = loss_function(y_pred, y_local)   # compute loss
-        loss.backward()                         # reeeeewind (backward pass)
-        optimizer.step()                        # parameter update
+        y_pred = model(X_local_minibatch)                # fwd the bass (forward pass)
+        loss = loss_function(y_pred, y_local_minibatch)  # compute loss
+        loss.backward()                                  # reeeeewind (backward pass)
+        optimizer.step()                                 # parameter update
 
         train_running_loss += loss.detach().item()
-        train_acc += get_accuracy(y_pred, y_local, model.batch_size)
+        train_acc += model.get_accuracy(y_pred, y_local_minibatch)
 
+    print(
+        "Epoch:  %d | NLLoss: %.4f | Train Accuracy: %.2f"
+        % (epoch, train_running_loss / num_batches, train_acc / num_batches)
+    )
 
-    #if epoch % 100 == 0:
-    print('Epoch:  %d | NLLoss: %.4f | Train Accuracy: %.2f'  %(epoch, train_running_loss /num_batches, train_acc/num_batches))
-    #hist[epoch] = loss_item
-
-
-print("Training loss: " + hist)
